6e38e24d9537b5e0ee706a98fe0dc8ad1ae1cd20
victorKimpel() <mars.2874@gmail.com>
2022-06-15 02:38:11 +0300

    miniGame_1.0

--- HomeWorks/HomeWorks/ViewController.swift
+++ HomeWorks/HomeWorks/ViewController.swift
@@ -8,15 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myStackView: UIStackView!
+    @IBOutlet weak var myView: UIView!
+    @IBOutlet weak var buttonOne: UIButton!
+    @IBOutlet weak var buttonTwo: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        myStackView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(stackDidTap)))
+        myView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(viewDidTap)))
+    }
+    
+    @objc func stackDidTap() {
+        myStackView.frame.origin = .init(x: .random(in: 0...view.frame.maxX - myStackView.frame.width), y: .random(in: 0...view.frame.maxY - myStackView.frame.height))
+    }
+    
+    @objc func viewDidTap() {
+        myView.frame = .init(x: myView.frame.origin.x, y: myView.frame.origin.y, width: 10, height: 10)
+    }
+    
+    @IBAction func actionOne(_ sender: Any) {
+        myStackView.isHidden = true
+    }
+    
+    @IBAction func actionTwo(_ sender: Any) {
+        myStackView.isHidden = false
+    }
 }
 
-class Victor {
-    var strong = 100
-}
