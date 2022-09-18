b14d5646972b724e2bcd33944b06b158b55eae15
victorKimpel() <mars.2874@gmail.com>
2022-09-19 01:54:41 +0300

    Trimmed and Capitalized

--- PropertyWrappers/PropertyWrappers/Capitalized.swift
+++ PropertyWrappers/PropertyWrappers/Capitalized.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+@propertyWrapper
+struct Capitalized {
+    
+    var wrappedValue: String {
+        didSet {
+            wrappedValue = wrappedValue.capitalized
+        }
+    }
+    init(wrappedValue: String) {
+        self.wrappedValue = wrappedValue.capitalized
+    }
+}
--- PropertyWrappers/PropertyWrappers/Trimmed.swift
+++ PropertyWrappers/PropertyWrappers/Trimmed.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+@propertyWrapper
+struct Trimmed {
+    private(set) var value: String = ""
+    
+    var wrappedValue: String {
+        get {
+            return value
+        }
+        set {
+            value = newValue.trimmingCharacters(in: .whitespacesAndNewlines)
+        }
+    }
+    
+    init(wrappedValue: String) {
+        self.wrappedValue = wrappedValue
+    }
+}
+
+struct Trim {
+    @Trimmed var value: String
+}
